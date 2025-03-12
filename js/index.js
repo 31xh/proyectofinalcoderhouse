ca8d008d93d78ef3fe7361337a3e4636e0fcb5ca
Sebastian Garcia <sebastiangarcia1105@gmail.com>
2025-03-11 21:44:32 -0300

    Cambios en el apartado de productos

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const video = document.querySelector("#video");
+    const text = document.querySelector(".video-text");
+
+    video.addEventListener("ended", () => {
+        
+        text.style.opacity = "1";
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const videos = document.querySelectorAll("video");
+    const animatedElements = document.querySelectorAll("[data-aos]");
+
+    animatedElements.forEach((element, index) => {
+        element.addEventListener("animationend", () => {
+            if (videos[index]) {
+                videos[index].play();
+                console.log(`🎥 Video ${index + 1} iniciado después de la animación.`);
+            }
+        });
+    });
+});
